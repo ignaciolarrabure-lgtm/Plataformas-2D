bebacd756380322c4e9fd83533302c75a2bddadb
Ignacio Larrabure <ignaciolarraburegmail.com>
2026-03-12 15:04:28 -0500

    Update:07 Doors

--- Assets/Scripts/Coin_Counter.cs
+++ Assets/Scripts/Coin_Counter.cs
@@ -7,6 +7,10 @@ public class Coin_Counter : MonoBehaviour
     public TMP_Text Coin_Text;
     public int Current_Coins = 0;
 
+    [Header("Door configuration")]
+    public int Total_Coins_In_Level; 
+    public GameObject Door_To_Destroy; 
+
     private void Awake()
     {
         instance = this;
@@ -14,20 +18,35 @@ public class Coin_Counter : MonoBehaviour
 
     private void Start()
     {
-        Coin_Text.text = "Coins : " + Current_Coins.ToString();
+        Total_Coins_In_Level = GameObject.FindObjectsByType<Coin_Collected>(FindObjectsSortMode.None).Length;
+
+        Update_UI();
     }
 
     public void Increase_Coins(int v)
     {
         Current_Coins += v;
-        Coin_Text.text = "Coins : " + Current_Coins.ToString();
+        Update_UI();
 
+        if (Current_Coins >= Total_Coins_In_Level)
+        {
+            Unlock_Door();
+        }
     }
 
+    void Update_UI()
+    {
+        Coin_Text.text = "Coins : " + Current_Coins.ToString();
+    }
 
-
-
-
+    void Unlock_Door()
+    {
+        if (Door_To_Destroy != null)
+        {
+            Destroy(Door_To_Destroy);
+            Debug.Log("Door opened");
+        }
+    }
 }
 
 
